8c341461aed1af42633cd36b59aed5b9ed9a8a65
Alexander Wesner <A.wesner@besecke.de>
2025-02-02 16:27:44 +0100

    Add employee service methods for qualifications.
    Create methods to obtain the qualifications of the selected employee.
    Add and remove employee qualifications.
    Delete a qualification from an employee master record.
    Getting all qualifications.
    Add a new qualification.

--- src/app/core/services/employee.service.ts
+++ src/app/core/services/employee.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, catchError, throwError, map } from 'rxjs';
 import { Employee } from '../../shared/models/employee';
+import { Qualification } from '../../shared/models/employee';
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
   private apiUrl = '/backend';
+  private baseUrl = environment.apiUrl;
   private httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
@@ -22,6 +25,24 @@ export class EmployeeService {
       .pipe(catchError(this.handleError));
   }
 
+  getEmployee(id: number): Observable<Employee> {
+    return this.http
+      .get<Employee>(`${this.apiUrl}/${id}`, this.httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+
+  updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http
+      .put<Employee>(`${this.baseUrl}/employees/${employee.id}`, employee, this.httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+
+  getEmployeeQualifications(id: number): Observable<Qualification[] | null> {
+    return this.http
+      .get<Qualification[] | null>(`${this.baseUrl}/employees/${id}/qualifications`, this.httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+
   searchEmployees(searchTerm: string): Observable<Employee[]> {
     return this.http.get<Employee[]>(this.apiUrl, this.httpOptions).pipe(
       map((employees) =>
@@ -33,6 +54,31 @@ export class EmployeeService {
       ),
       catchError(this.handleError)
     );
+  }
+
+  getAllQualifications(): Observable<Qualification[]> {
+    return this.http.get<Qualification[]>(`${this.baseUrl}/qualifications`, this.httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+
+  addQualification(skill: string): Observable<Qualification> {
+    return this.http.post<Qualification>(`${this.baseUrl}/qualifications`, {skill: skill}, this.httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+
+  deleteQualification(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/qualifications/${id}`, this.httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+
+  addQualificationToEmployee(employeeId: number, skill: string): Observable<any> {
+    return this.http.post(`${this.baseUrl}/employees/${employeeId}/qualifications`, { skill }, this.httpOptions)
+      .pipe(catchError(this.handleError));
+  }
+
+  deleteQualificationFromEmployee(employeeId: number, qualificationId: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/employees/${employeeId}/qualifications/${qualificationId}`, this.httpOptions)
+      .pipe(catchError(this.handleError))
   }
 
   private handleError(error: any) {
